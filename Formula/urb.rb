da6ef9cf50b317d8440d8536b63e9ccb90e56986
Sean Goedecke <seangoedecke@gmail.com>
2018-01-06 21:15:32 +1100

    use zip instead

--- Formula/urb.rb
+++ Formula/urb.rb
@@ -1,9 +1,9 @@
 class Urb < Formula
   desc "Urban Dictionary command line tool"
   homepage "https://github.com/sgoedecke/urb"
-  url "https://github.com/sgoedecke/urb/blob/master/urb-0.0.1.tar.gz"
+  url "https://github.com/sgoedecke/urb/blob/master/urb-0.0.1.zip"
   version "0.0.1"
-  sha256 "74f3f46ba7e8ac96cce7e1ef41b11505ec3b77bee05506b42dd866bd18934034"
+  sha256 "59f0ef90f00120a5e2a5b57ecb5ab291d66d7e2100469ffc6ca1d0d35fa9a51a"
 
   def install
     bin.install "urb"
