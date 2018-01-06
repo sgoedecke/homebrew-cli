e83dfe46143221a1a91ad0a1f65bde2534fa5732
Sean Goedecke <seangoedecke@gmail.com>
2018-01-06 21:08:56 +1100

    update checksum

--- Formula/urb.rb
+++ Formula/urb.rb
@@ -3,7 +3,7 @@ class Urb < Formula
   homepage "https://github.com/sgoedecke/urb"
   url "https://github.com/sgoedecke/urb/blob/master/urb-0.0.1.tar.gz"
   version "0.0.1"
-  sha256 "481c6f0c3d672fdb2cf7efba708276b6e1eb92eee9b1d354122e7631bfb76893"
+  sha256 "74f3f46ba7e8ac96cce7e1ef41b11505ec3b77bee05506b42dd866bd18934034"
 
   def install
     bin.install "urb"
