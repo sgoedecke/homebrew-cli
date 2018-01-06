69b0bd0104d3110eefeea40d1892e4afbf730dc8
Sean Goedecke <seangoedecke@gmail.com>
2018-01-06 21:40:17 +1100

    Revert "shot in the dark"
    This reverts commit dae47919def7b467bb462d0197eee6d39d859a33.

--- Formula/urb.rb
+++ Formula/urb.rb
@@ -1,8 +1,7 @@
 class Urb < Formula
   desc "Urban Dictionary command line tool"
   homepage "https://github.com/sgoedecke/urb"
-  # url "https://raw.githubusercontent.com/sgoedecke/urb/master/urb-0.0.1.tar.gz"
-  url "https://google.com"
+  url "https://raw.githubusercontent.com/sgoedecke/urb/master/urb-0.0.1.tar.gz"
   version "0.0.1"
   sha256 "0f45cacea4f343d8b78badb613572f08153fc07f9a2fc476d9889b9b88c8f8be"
 
