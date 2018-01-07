9b414cee56013d9b944bd325e394f386e1f5b5ee
Sean Goedecke <seangoedecke@gmail.com>
2018-01-07 14:57:18 +1100

    add formula for geekbot

---
+++ Formula/geekbotbot.rb
@@ -0,0 +1,12 @@
+class GeekBotBot < Formula
+  desc "Generate random standup notes from Geekbot Slack logs"
+  homepage "https://github.com/sgoedecke/markov-standup"
+  url "https://raw.githubusercontent.com/sgoedecke/markov-standup/master/geekbotbot-1.0.0.tar.gz"
+  version "1.0.0"
+  sha256 "9f16c83eedc96517f5d06a455b56af2bb05f952e2d77b3d5b5db0e04039f5a86"
+
+  def install
+    libexec.install Dir["*"]
+    bin.write_exec_script (libexec/"standup")
+  end
+end
